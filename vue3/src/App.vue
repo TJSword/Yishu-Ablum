b8ef8382bf11a835b9b3cb45efb60c703f0384db
TJSword <luckydream@vip.qq.com>
2023-04-13 22:55:34 +0800

    新增：前端工程

---
+++ vue3/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app_wrap">
+      <div id="header">
+        <div class="title">YiShu's album</div>
+        <div class="tools">
+           <div class="avator"></div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style  lang="less" scoped>
+#app_wrap {
+  height: 100%;
+  width: 100%;
+  background: #FFF8ED;
+  #header{
+    height: 50px;
+    width: 100%;
+    background: #F4F5F0;
+    line-height: 50px;
+    display: flex;
+    justify-content: space-between;
+    .title{
+      font-weight: 700;
+      margin-left: 40px;
+      font-size: 18px;
+    }
+  }
+}
+</style>
